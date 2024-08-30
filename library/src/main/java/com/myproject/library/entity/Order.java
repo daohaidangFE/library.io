772bcaf3c44ccaafd09a8cdcda2dcdfb18b1f18f
Dao Hai Dang <daohaidangpc@gmail.com>
2024-08-30 20:52:22 +0700

    order rename to purchase

--- library/src/main/java/com/myproject/library/entity/Order.java
+++ library/src/main/java/com/myproject/library/entity/Order.java
@@ -13,6 +13,7 @@ import java.util.Date;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Table(name = "purchase")
 public class Order {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
