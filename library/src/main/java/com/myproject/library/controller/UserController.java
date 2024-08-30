fbb505a1206e5ffaf28351ca456a852d18c8ccc9
Dao Hai Dang <daohaidangpc@gmail.com>
2024-08-30 10:53:13 +0700

    push

---
+++ library/src/main/java/com/myproject/library/controller/UserController.java
@@ -0,0 +1,4 @@
+package com.myproject.library.controller;
+
+public class UserController {
+}
---
+++ library/src/main/java/com/myproject/library/entity/User.java
@@ -0,0 +1,9 @@
+package com.myproject.library.entity;
+
+
+import jakarta.persistence.Entity;
+
+@Entity
+
+public class User {
+}
